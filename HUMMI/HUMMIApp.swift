5c8b9ea607c20077e234094a14cba44f097899af
kirtidhwaj <kirtidhwajpatra@gmail.com>
2026-07-05 16:59:37 +0530

    Initial Commit

---
+++ HUMMI/HUMMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HUMMIApp.swift
+//  HUMMI
+//
+//  Created by Uday on 05/07/26.
+//
+
+import SwiftUI
+
+@main
+struct HUMMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
